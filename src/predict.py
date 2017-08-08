798adfce46b4618768aa2ad0d9bacfcb9fea6f43
Egor Panfilov <egor.v.panfilov@gmail.com>
2017-08-08 09:32:24 +0000

    Added 224x336 for predict, threholding for bce

--- src/predict.py
+++ src/predict.py
@@ -9,8 +9,8 @@ import scipy.ndimage as ndi
 import cv2
 
 # from h5py_shuffled_batch_gen import Dataset
-from models.unet_zf import ZF_UNET_224, ZF_UNET_320_480
-from models.unet_deform import UNET_DEFORM_224
+from models.unet_zf import UNET_ZF_224, UNET_320_480
+from models.unet_deform import UNET_DEFORM_224, UNET_DEFORM_224_336
 from utils import (_resize_encode_mask,
                    _mask_to_rle_string)
 
@@ -39,9 +39,10 @@ if __name__ == '__main__':
     if not os.path.exists(config.path_masks_low):
         os.makedirs(config.path_masks_low)
 
-    # model = ZF_UNET_224()
-    # model = ZF_UNET_320_480()
-    model = UNET_DEFORM_224()
+    # model = UNET_ZF_224()
+    # model = UNET_320_480()
+    # model = UNET_DEFORM_224()
+    model = UNET_DEFORM_224_336()
     model.load_weights(config.weights)
 
     fnames = glob.glob(os.path.join(config.path_imgs, '*'))
@@ -78,13 +79,17 @@ if __name__ == '__main__':
     struct_rep = ndi.morphology.iterate_structure(struct, 4)
 
     def job_postprocess(image_bin):
-        tmp = ndi.morphology.binary_erosion(image_bin, struct_rep)
+        tmp = image_bin
+        # tmp = ndi.morphology.binary_erosion(image_bin, struct_rep)
         # tmp = ndi.morphology.binary_opening(image_bin, struct3)
         # image_filt = ndi.morphology.binary_closing(image_filt, struct3)
         return tmp
 
     def job_binarize_postproc_upsize(image):
-        image_bin = np.not_equal(image, 0)
+        # For `dice loss`
+        # image_bin = np.not_equal(image, 0)
+        # For `binary cross-entropy loss`
+        image_bin = image > 0.7
         image_filt = job_postprocess(image_bin)
         image_up = cv2.resize(image_filt.astype(np.uint8), shape_out[::-1])
         image_up = np.not_equal(image_up, 0)
@@ -96,6 +101,11 @@ if __name__ == '__main__':
     def job_file_mask_low_to_rle(mask):
         tmp = job_read_mask(mask)
         tmp = job_binarize_postproc_upsize(tmp)
+        tmp = job_mask_to_rle(tmp)
+        return tmp
+
+    def job_mask_low_to_rle(mask):
+        tmp = job_binarize_postproc_upsize(mask)
         tmp = job_mask_to_rle(tmp)
         return tmp
 
@@ -119,24 +129,26 @@ if __name__ == '__main__':
 
             # Save predicts
             # print('...Batch save predicts')
-            batch_fnames_out = [os.path.join(config.path_masks_low, e)
-                                for e in batch_bnames]
-            parallel(delayed(job_write_mask)(f, m)
-                     for f, m in zip(batch_fnames_out,
-                                     batch_masks_low))
+            if False:
+                batch_fnames_out = [os.path.join(config.path_masks_low, e)
+                                    for e in batch_bnames]
+                parallel(delayed(job_write_mask)(f, m)
+                         for f, m in zip(batch_fnames_out,
+                                         batch_masks_low))
             # *****************************************************************
 
             # *****************************************************************
-            # XXX
-            # batch_fnames_out = batch_fnames_in
-
             # Read, upscale, and apply RLE
-            print('...Batch read encode')
-            rles = parallel(delayed(job_file_mask_low_to_rle)(e)
-                            for e in batch_fnames_out)
+            # print('...Batch read encode')
+            if False:
+                rles = parallel(delayed(job_file_mask_low_to_rle)(e)
+                                for e in batch_fnames_out)
+            rles = parallel(delayed(job_mask_low_to_rle)(e)
+                            for e in batch_masks_low)
 
             # Aggregate the results
-            results.update(zip(batch_bnames, rles))
+            batch_bnames_o = [e.replace('.jpg', '.png') for e in batch_bnames]
+            results.update(zip(batch_bnames_o, rles))
             # *****************************************************************
 
             if idx_intvl % 10 == 0:
